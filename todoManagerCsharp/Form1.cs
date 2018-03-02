58bf1916d66d1a2026451cd6e0e3b99e6fa433d6
jhd235 <jhd235@outlook.com>
2018-03-02 15:59:48 +0500

    first working dummy

--- todoManagerCsharp/Form1.cs
+++ todoManagerCsharp/Form1.cs
@@ -15,6 +15,22 @@ namespace todoManagerCsharp
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox1.Text = "hh";
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            label1.Text = "";
         }
     }
 }
